23beade5f8f451c1ae1177414e24462535b360e8
ehrenschwan <luca@ehrenschwan.dev>
2023-11-29 20:12:14 +0100

    Setup for december 1st

---
+++ README.md
@@ -0,0 +1,16 @@
+# AOC 2023
+
+By default the aoc23 bin is run, it produces this usage:
+
+```
+These are my solutions for aoc 2023.
+Each day is it's own binary so you can run it using:
+  cargo run --bin day[1-25]
+
+Each day also has tests using the examples.
+These can be run using:
+  cargo test
+
+Or a single day with:
+  cargo test --bin day[1-25]
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    println!("These are my solutions for aoc 2023.");
+    println!("Each day is it's own binary so you can run it using:");
+    println!("  cargo run --bin day[1-25]");
+    println!("");
+    println!("Each day also has tests using the examples.");
+    println!("These can be run using:");
+    println!("  cargo test --bins");
+    println!("");
+    println!("Or a single day with:");
+    println!("  cargo test --bin day[1-25]");
 }
