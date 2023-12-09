25b75d8af4981275092f93ad093992f9b556790e
ehrenschwan <luca@ehrenschwan.dev>
2023-12-09 14:26:30 +0100

    Added parsing lines of nums

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use num::Integer;
 use std::str::FromStr;
 
 pub fn read_one_per_line<T>(path: &str) -> Result<Vec<T>>
@@ -13,4 +14,18 @@ where
 
 pub fn load_input(path: &str) -> Result<String> {
     Ok(std::fs::read_to_string(path)?)
+}
+
+pub fn read_lines_of_num<T>(path: &str) -> Result<Vec<Vec<T>>>
+where
+    T: Integer + FromStr,
+{
+    Ok(std::fs::read_to_string(path)?
+        .lines()
+        .map(|line| {
+            line.split(" ")
+                .filter_map(|value| value.parse::<T>().ok())
+                .collect()
+        })
+        .collect())
 }
