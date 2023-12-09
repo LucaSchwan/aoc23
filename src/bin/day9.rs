37c6531ab0229b8ef06c667c0992be39e2453bec
ehrenschwan <luca@ehrenschwan.dev>
2023-12-09 14:26:40 +0100

    Refactored to using itertools tuple_windows

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,41 +1,13 @@
-use std::str::FromStr;
+use anyhow::Result;
+use itertools::Itertools;
 use std::vec::Vec;
 
-use anyhow::{Error, Result};
-
-#[derive(Debug, Clone)]
-struct ValueHistory {
-    history: Vec<i32>,
-}
-
-impl FromStr for ValueHistory {
-    type Err = Error;
-
-    fn from_str(s: &str) -> std::prelude::v1::Result<Self, Self::Err> {
-        Ok(ValueHistory {
-            history: s
-                .split(" ")
-                .map(|value| value.parse::<i32>().expect("Should be a number"))
-                .collect(),
-        })
-    }
-}
-
-impl FromIterator<i32> for ValueHistory {
-    fn from_iter<T: IntoIterator<Item = i32>>(iter: T) -> Self {
-        ValueHistory {
-            history: Vec::from_iter(iter),
-        }
-    }
-}
-
-fn build_tree(history: ValueHistory) -> Vec<ValueHistory> {
+fn build_tree(history: Vec<i32>) -> Vec<Vec<i32>> {
     let mut values_tree = vec![history];
 
     while !values_tree
         .last()
         .expect("Last should exist")
-        .history
         .iter()
         .all(|value| *value == 0)
     {
@@ -44,10 +16,9 @@ fn build_tree(history: ValueHistory) -> Vec<ValueHistory> {
             .iter()
             .last()
             .expect("Should have a last")
-            .history
-            .as_slice()
-            .windows(2)
-            .map(|window| window[1] - window[0])
+            .iter()
+            .tuple_windows()
+            .map(|(left, right)| right - left)
             .collect();
 
         values_tree.push(new_values);
@@ -56,31 +27,29 @@ fn build_tree(history: ValueHistory) -> Vec<ValueHistory> {
     values_tree
 }
 
-fn calc_extrapolate_forwards(history: ValueHistory) -> i32 {
+fn calc_extrapolate_forwards(history: Vec<i32>) -> i32 {
     let mut values_tree = build_tree(history);
 
-    values_tree
-        .iter_mut()
-        .for_each(|values| values.history.reverse());
+    values_tree.iter_mut().for_each(|values| values.reverse());
     values_tree.reverse();
 
     values_tree.iter().skip(1).fold(0, |value, history| {
-        value + *history.history.first().expect("Should have first")
+        value + *history.first().expect("Should have first")
     })
 }
 
-fn calc_extrapolate_backwards(history: ValueHistory) -> i32 {
+fn calc_extrapolate_backwards(history: Vec<i32>) -> i32 {
     let mut values_tree = build_tree(history);
 
     values_tree.reverse();
 
     values_tree.iter().skip(1).fold(0, |value, values| {
-        *values.history.first().expect("Should have first") - value
+        *values.first().expect("Should have first") - value
     })
 }
 
 fn part1(path: &str) -> Result<i32> {
-    let histories = aoc23::read_one_per_line::<ValueHistory>(path)?;
+    let histories = aoc23::read_lines_of_num::<i32>(path)?;
 
     Ok(histories
         .into_iter()
@@ -89,7 +58,7 @@ fn part1(path: &str) -> Result<i32> {
 }
 
 fn part2(path: &str) -> Result<i32> {
-    let histories = aoc23::read_one_per_line::<ValueHistory>(path)?;
+    let histories = aoc23::read_lines_of_num::<i32>(path)?;
 
     Ok(histories
         .into_iter()
