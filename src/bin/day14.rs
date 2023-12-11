3b3b90731c214e47c7ed532c61e6edce833deae0
ehrenschwan <luca@ehrenschwan.dev>
2023-12-11 12:15:20 +0100

    New template for days

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,13 +1,30 @@
+use anyhow::Result;
+
+fn part1(path: &str) -> Result<u32> {
+    todo!()
+}
+
+fn part2(path: &str) -> Result<u32> {
+    todo!()
+}
+
 fn main() {
     todo!();
+    println!("Part1: {}", part1("data/x.input").unwrap());
+    println!("Part1: {}", part2("data/x.input").unwrap());
 }
 
 #[cfg(test)]
 mod test {
-    // use super::*;
+    use super::*;
 
     #[test]
-    fn test() {
-        assert!(true);
+    fn part1_test() {
+        assert_eq!(part1("data/x.sample").unwrap(), 0);
+    }
+
+    #[test]
+    fn part2_test() {
+        assert_eq!(part2("data/x.sample").unwrap(), 0);
     }
 }
